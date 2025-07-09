de836dc5a0a337a651204833d657eab003f4813e
choijin <cjg030820@gmail.com>
2025-07-09 18:11:06 +0900

    db js 작업

--- scripts/db.js
+++ scripts/db.js
@@ -1,62 +1,62 @@
 const newArrivalsWoman = [
     {
-        src:'',
+        src:'./images/new/new001.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new002.png',
         gender:'woman',
         name:'snap that',
         kind:'싱글 스트랩 원피스',
         price:77000,
     },
     {
-        src:'',
+        src:'./images/new/new003.png',
         gender:'woman',
         name:'blue viper',
         kind:'스트랩 원피스',
         price:77000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'wing streak',
         kind:'반신복 니스킨',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/new/new004.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
@@ -65,63 +65,63 @@ const newArrivalsWoman = [
 ]
 const weekBestWoman = [
     {
-        src:'',
+        src:'./images/week/week001.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week002.png',
         gender:'woman',
         name:'snap that',
         kind:'싱글 스트랩 원피스',
         price:77000,
     },
     {
-        src:'',
+        src:'./images/week/week003.png',
         gender:'woman',
         name:'blue viper',
         kind:'스트랩 원피스',
         price:77000,
     },
     {
-        src:'',
+        src:'./images/week/week004.png',
         gender:'woman',
         name:'wing streak',
         kind:'반신복 니스킨',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week005.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week005.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week005.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week005.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
         price:82000,
     },
     {
-        src:'',
+        src:'./images/week/week005.png',
         gender:'woman',
         name:'sitting duck',
         kind:'다이아몬드 원피스',
@@ -130,29 +130,53 @@ const weekBestWoman = [
 ]
 const trand = [
     {
-        src:'',
+        src:'./images/trand/trand001.png',
         title:'수많은 수영선수들의 PICK'
     },
     {
-        src:'',
+        src:'./images/trand/trand002.png',
         title:'최신 남성 트렌드 웨어 추천'
     },
     {
-        src:'',
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
+        title:'인기 급상승 여성 수영복 추천'
+    },
+    {
+        src:'./images/trand/trand003.png',
         title:'인기 급상승 여성 수영복 추천'
     },
 ]
 const news = [
     {
-        src:'',
+        src:'./images/fun_news/fun_news001.png',
         date:'2025.06.18',
         title:'사라 라크, 5번의 수술에도 다시 레일위에 올라 화제'
     },{
-        src:'',
+        src:'./images/fun_news/fun_news002.png',
         date:'2025.06.17',
         title:'월드 챔피언쉽의 열기를 고조시키는 호주의 선수들'
     },{
-        src:'',
+        src:'./images/fun_news/fun_news003.png',
         date:'2025.06.14',
         title:'역대 14세 100m 선수중 가장 빠른 기록 달성하였던 제이콥 휘틀,충격 근황 밝혀져'
     },
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,39 @@
+const aAll = document.querySelectorAll('a')
+for(i = 0 ; i < 37 ; i++){
+    aAll[i].addEventListener('click',(e)=>{
+        e.preventDefault()
+    })
+}
 const newsList = document.querySelectorAll('.news_list ul li')
 for(i = 0 ; i < 3 ; i++){
     const newsContent = document.createElement('a')
     newsList[i].appendChild(newsContent)
-    newsContent.innerHTML = `<img src="#" alt="#">`
+    newsContent.innerHTML = `<img src="${news[i].src}" alt="#">`
     newsContent.innerHTML += `<span class="date">${news[i].date}</span>`
     newsContent.innerHTML += `<span class="title">${news[i].title}</span>`
-    console.log(newsContent)
+}
+const cateBtn = document.querySelectorAll('.cate_btn')
+for(let i = 0 ; i < 7 ; i++){
+    cateBtn[0].children[0].children[0].classList.add('active')
+    cateBtn[0].children[i].children[0].addEventListener('click', ()=>{
+        cateBtn[0].children[0].children[0].classList.remove('active')
+        cateBtn[0].children[1].children[0].classList.remove('active')
+        cateBtn[0].children[2].children[0].classList.remove('active')
+        cateBtn[0].children[3].children[0].classList.remove('active')
+        cateBtn[0].children[4].children[0].classList.remove('active')
+        cateBtn[0].children[5].children[0].classList.remove('active')
+        cateBtn[0].children[6].children[0].classList.remove('active')
+        cateBtn[0].children[i].children[0].classList.add('active')
+    })
+    cateBtn[1].children[0].children[0].classList.add('active')
+    cateBtn[1].children[i].children[0].addEventListener('click', ()=>{
+        cateBtn[1].children[0].children[0].classList.remove('active')
+        cateBtn[1].children[1].children[0].classList.remove('active')
+        cateBtn[1].children[2].children[0].classList.remove('active')
+        cateBtn[1].children[3].children[0].classList.remove('active')
+        cateBtn[1].children[4].children[0].classList.remove('active')
+        cateBtn[1].children[5].children[0].classList.remove('active')
+        cateBtn[1].children[6].children[0].classList.remove('active')
+        cateBtn[1].children[i].children[0].classList.add('active')
+    })
 }
